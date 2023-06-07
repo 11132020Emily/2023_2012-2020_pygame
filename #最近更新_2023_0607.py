7e8f547a05e9c983c8e639d01ee0dd89913c9b38
11132020Emily <d11132020@gapps.fg.tp.edu.tw>
2023-06-07 21:00:50 +0800

    testing_photo_uploading

--- #最近更新_2023_0607.py
+++ #最近更新_2023_0607.py
@@ -1,2 +1,26 @@
 #最近更新
-a = 1
+import pygame as pg
+
+#pygame初始化
+pg.init()
+
+#設定視窗的寬&高
+width, height = 1000, 800
+screen = pg.display.set_mode((width, height))
+
+#設定視窗標題
+pg.display.set_caption("Emily's_test")
+#image.load('圖片路徑')
+imm = pg.image.load('player1.png')
+#視窗變數.blit(圖像, 繪製位置)
+screen.blit(imm, (0, 0))
+
+running = True
+while running:
+    for event in pg.event.get():
+        #若點按關閉按鈕
+        if event.type == pg.QUIT:
+            #停止使用pygame
+            pg.quit()
+    #更新視窗內容
+    pg.display.flip()
